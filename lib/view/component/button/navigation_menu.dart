039acdce2e48faf3077651d06f29f53089637a38
fikrilal <fikrimuzakky24@gmail.com>
2023-11-20 16:57:11 +0700

    not responsive yet

--- lib/view/component/button/navigation_menu.dart
+++ lib/view/component/button/navigation_menu.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hicons/flutter_hicons.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:iconly/iconly.dart';
 import 'package:lelewise_mobile_apps/controller/navigation_controller.dart';
 import 'package:lelewise_mobile_apps/res/colors/color_libraries.dart';
 import 'package:lelewise_mobile_apps/view/page/air/suhu_air_dashboard.dart';
@@ -9,6 +12,20 @@ import 'package:lelewise_mobile_apps/view/page/pH/pH_dashboard.dart';
 import 'package:lelewise_mobile_apps/view/page/pakan/pakan_dashboard.dart';
 import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
+class CenterDockedFloatingActionButtonLocation extends FloatingActionButtonLocation {
+  @override
+  Offset getOffset(ScaffoldPrelayoutGeometry scaffoldGeometry) {
+    double fabX = (scaffoldGeometry.scaffoldSize.width - scaffoldGeometry.floatingActionButtonSize.width) / 2;
+    double fabY = scaffoldGeometry.contentBottom;
+
+    return Offset(fabX, fabY);
+  }
+}
+
+
+
+
+
 class NavigationMenu extends StatelessWidget {
   NavigationMenu({super.key});
 
@@ -16,7 +33,6 @@ class NavigationMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: PageView(
         onPageChanged: controller.animateToTab,
@@ -33,16 +49,51 @@ class NavigationMenu extends StatelessWidget {
         notchMargin: 10,
         elevation: 0,
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 35, vertical: 15),
+          padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 12.h),
           child: Obx(
-            () => Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                () => Row(
               children: [
-                _bottomAppBarItem(context, icon: Hicons.home_3_light_outline, activeIcon: Hicons.home_3_bold, page: 0, label: "Home"),
-                _bottomAppBarItem(context, icon: Hicons.drop_light_outline, activeIcon: Hicons.drop_bold, page: 1, label: "Pakan"),
-                _bottomAppBarItem(context, icon: Hicons.discovery_2_light_outline, activeIcon: Hicons.discovery_2_bold, page: 2, label: "Suhu"),
-                _bottomAppBarItem(context, icon: Hicons.virus_light_outline, activeIcon: Hicons.virus_bold, page: 3, label: "pH"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // KENAPA SPACE BETWEEN PADA ROW INI TIDAK BEKERJA
+                  children: [
+                    _bottomAppBarItem(context, icon: Hicons.home_3_light_outline, activeIcon: Hicons.home_3_bold, page: 0, label: "Home"),
+                    _bottomAppBarItem(context, icon: Hicons.drop_light_outline, activeIcon: Hicons.drop_bold, page: 1, label: "Pakan"),
+                  ],
+                ),
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // SPACE BETWEEN PADA ROW INI JUGA TIDAK BEKERJA
+                  children: [
+                    _bottomAppBarItem(context, icon: Hicons.discovery_2_light_outline, activeIcon: Hicons.discovery_2_bold, page: 2, label: "Suhu"),
+                    _bottomAppBarItem(context, icon: Hicons.virus_light_outline, activeIcon: Hicons.virus_bold, page: 3, label: "pH"),
+                  ],
+                ),
+
               ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: CenterDockedFloatingActionButtonLocation(),
+      floatingActionButton: ZoomTapAnimation(
+        onTap: () {
+          // Add your action when the button is pressed
+        },
+        child: Transform.scale(
+          scale: 2, // Adjust the scale factor as needed
+          child: FloatingActionButton(
+            elevation: 0,
+            onPressed: () {
+              // Add your action when the button is pressed
+            },
+            backgroundColor: ListColor.primary,
+            mini: true,
+            child: SvgPicture.asset(
+              'assets/icons/Scan.svg',
+              width: 20,
+              height: 20,
             ),
           ),
         ),
@@ -59,18 +110,19 @@ class NavigationMenu extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Icon(
-            controller.currentPage.value == page ? activeIcon : icon,
-            color: controller.currentPage.value == page ? ListColor.primary : ListColor.gray400),
-            Text(label,
-            style: TextStyle(
-              color: controller.currentPage.value == page ? ListColor.primary : ListColor.gray400,
-              fontSize: 15,
-              fontWeight: controller.currentPage.value == page ? FontWeight.w600 : FontWeight.w500,
-            ),)
+                controller.currentPage.value == page ? activeIcon : icon,
+                color: controller.currentPage.value == page ? ListColor.primary : ListColor.gray400),
+            Text(
+              label,
+              style: TextStyle(
+                color: controller.currentPage.value == page ? ListColor.primary : ListColor.gray400,
+                fontSize: 15,
+                fontWeight: controller.currentPage.value == page ? FontWeight.w600 : FontWeight.w500,
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
