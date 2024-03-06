8d93bb0fc33802b94ece3555e4fab1a5bd36dfb8
fikrilal <fikrimuzakky24@gmail.com>
2024-03-06 19:36:37 +0700

    update

--- lib/view/page/monitoring/monitoring_dashboard.dart
+++ lib/view/page/monitoring/monitoring_dashboard.dart
@@ -5,6 +5,10 @@ import 'package:intl/intl.dart';
 import 'package:lelewise_mobile_apps/view/component/card/card_ph.dart';
 import 'package:lelewise_mobile_apps/view/component/card/card_suhu.dart';
 
+
+import '../../../controller/ph_suhu/get_history_ph_suhu.dart';
+import '../../../controller/realtime_data/get_ph_temperature.dart';
+import '../../../models/notification/notification_model.dart';
 import '../../../res/colors/color_libraries.dart';
 import '../../component/text/component_desc.dart';
 import '../../component/text/component_header.dart';
@@ -23,14 +27,70 @@ class _MonitoringDashboardState extends State<MonitoringDashboard> {
   String suhuMessage = "Memuat data suhu...";
   String phCondition = "unknown";
   String suhuCondition = "unknown";
+  String universalMessage = "Memuat data...";
+
+  void initState() {
+    super.initState();
+    WidgetsFlutterBinding.ensureInitialized().addPostFrameCallback((_) {});
+    GetPHandTemperature getPHandTemperature = GetPHandTemperature();
+
+    final notificationModel = NotificationModel(onMessagePHUpdate: (message) {
+      setState(() {
+        phMessage = message;
+        if (message == "Terlalu tinggi") {
+          phCondition = "high";
+        } else if (message == "Kondisi baik") {
+          phCondition = "good";
+        } else {
+          phCondition = "low";
+        }
+      });
+    }, onMessageSuhuUpdate: (message) {
+      setState(() {
+        suhuMessage = message;
+        if (message == "Terlalu tinggi") {
+          suhuCondition = "high";
+        } else if (message == "Kondisi baik") {
+          suhuCondition = "good";
+        } else {
+          suhuCondition = "low";
+        }
+      });
+    }, onUniversalMessageUpdate: (message) {
+      setState(() => universalMessage = message);
+    });
+
+    getPHandTemperature.getPHStream().listen((newPh) {
+      double formattedPh = double.parse(newPh.toStringAsFixed(2));
+      setState(() {
+        ph = formattedPh;
+      });
+      notificationModel.handlePHChange(formattedPh);
+    });
+
+    getPHandTemperature.getTemperatureStream().listen((newTemperature) {
+      double formattedTemperature =
+      double.parse(newTemperature.toStringAsFixed(1));
+      setState(() {
+        suhu = formattedTemperature;
+      });
+      notificationModel.handleTemperatureChange(formattedTemperature);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.fromLTRB(16.w, 46.h, 16.w, 20.h),
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
+        width: MediaQuery
+            .of(context)
+            .size
+            .width,
+        height: MediaQuery
+            .of(context)
+            .size
+            .height,
         decoration: const BoxDecoration(color: Colors.white),
         child: SingleChildScrollView(
           child: Column(
@@ -43,8 +103,7 @@ class _MonitoringDashboardState extends State<MonitoringDashboard> {
                         ph: ph, phMessage: phMessage, phCondition: phCondition),
                   ),
                   Expanded(
-                    child: SuhuCard(
-                        suhu: suhu,
+                    child: SuhuCard(suhu: suhu,
                         suhuMessage: suhuMessage,
                         suhuCondition: suhuCondition),
                   )
@@ -53,23 +112,48 @@ class _MonitoringDashboardState extends State<MonitoringDashboard> {
               SizedBox(height: 16.h),
               ComponentTextTitleCenter("Riwayat pH & Suhu Air"),
               SizedBox(height: 20.h),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TextDescription16("pastDates[index]"),
-                  SizedBox(height: 10.h),
-                  Row(
-                    children: [
-                      TextDescriptionBold("Ini temperatur"),
-                      const Spacer(),
-                      TextDescriptionBold("Ini pH"),
-                    ],
-                  )
-                ],
-              ),
-              SizedBox(height: 12.h),
-              const Divider(
-                color: ListColor.gray200,
+              FutureBuilder<List<PHAndTemperatureHistory>>(
+                future: HistoryService.fetchPHAndTemperatureHistory(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Text("Error: ${snapshot.error}");
+                  } else if (snapshot.hasData) {
+                    var data = snapshot.data!;
+                    var pastDates = generatePastDates(
+                        DateTime.now(), data.length);
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 10.h),
+                            TextDescription16(pastDates[index]),
+                            SizedBox(height: 10.h),
+                            Row(
+                              children: [
+                                TextDescriptionBold(
+                                    "${data[index].temperature.toStringAsFixed(
+                                        0)}°C"),
+                                Spacer(),
+                                TextDescriptionBold(
+                                    "pH ${data[index].ph.toStringAsFixed(1)}"),
+                              ],
+                            ),
+                            SizedBox(height: 10.h),
+                            const Divider(color: ListColor.gray200),
+                          ],
+                        );
+                      },
+                    );
+                  } else {
+                    return Text("No data available");
+                  }
+                },
               ),
             ],
           ),
@@ -78,11 +162,11 @@ class _MonitoringDashboardState extends State<MonitoringDashboard> {
     );
   }
 
-  List<String> generatePastDates(DateTime startDate, int days) {
+  List<String> generatePastDates(DateTime startDate, int count) {
     List<String> dates = [];
     DateFormat dateFormat = DateFormat('EEEE, dd MMMM', 'id_ID');
 
-    for (int i = 0; i < days; i++) {
+    for (int i = 0; i < count; i++) {
       DateTime currentDate = startDate.subtract(Duration(days: i));
       dates.add(dateFormat.format(currentDate));
     }
